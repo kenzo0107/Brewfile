534e48e21a81c50f848274ce65188d01838ed677
kenzo0107 <kenzo.tanaka0107@gmail.com>
2015-02-28 23:26:05 +0900

    update

--- brewfile.sh
+++ brewfile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh# Make sure using latest Homebrew
+#!/bin/shbrew  Make sure using latest Homebrew
 brew update
 
 # Update already-installed formula
@@ -26,6 +26,9 @@ brew install mobile-shell
 # Git
 brew install git
 brew install hub
+brew install gitsh
+brew install gist
+brew install tig
 brew install gibo
 
 # Utils
@@ -35,13 +38,15 @@ brew install jenkins
 brew install openssl
 brew install libtool
 brew install cmake
-brew install autoconf
+brew install automake
 
 # Languages
 brew install rbenv
 brew install ruby-build
 brew cask install java
 
+brew install md5sha1sum
+brew install nkf
 brew install sl
 brew install ctags
 brew install tmux
@@ -49,15 +54,19 @@ brew install fish
 brew install automeke
 brew install ag
 brew install z
+brew install php53-redis
+brew install redis
 
 # DevOps
 brew install docker
+brew cask install boot2docker
 brew cask install packer
 
 # Casks
 brew cask install iterm2
 brew cask install google-chrome
 brew cask install google-drive
+brew cask install google-japanese-ime
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install kobito
@@ -66,10 +75,15 @@ brew cask install evernote
 brew cask install xtrafinder
 brew cask install github
 brew cask install lastpass-universal
+brew cask install totalspaces2
 brew cask install svnx
 brew cask install filezilla
 brew cask install firefox
+brew cask install openoffice
 brew cask install textmate
+brew cask install mamp
+brew cask install sourcetree
+brew cask install slack
 
 #cask install phpstorm
 #cask install rubymine
