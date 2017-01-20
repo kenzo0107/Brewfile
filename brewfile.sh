8b475084df2ed2398fcdb8e102d5cefbfea2b328
kenzo_tanaka <kenzo_tanaka@kenzo-tanaka-no-MacBook-Pro.local>
2017-01-20 16:35:31 +0900

    Update: latest

--- brewfile.sh
+++ brewfile.sh
@@ -4,9 +4,9 @@ brew update
 # Update already-installed formula
 brew upgrade
 
-
 # Add Repository
 brew tap homebrew/versions
+
 # Caskを追加したため向き先を自身のRepositoryに
 brew tap phinze/homebrew-cask
 #tap vexus2/homebrew-cask
@@ -21,7 +21,7 @@ brew install brew-cask
 # Shell
 brew install zsh
 brew install zsh-completions
-brew install mobile-shell
+#brew install mobile-shell
 
 # Git
 #brew install git
@@ -32,51 +32,55 @@ brew install mobile-shell
 #brew install gibo
 
 # Utils
-brew install wget
-brew install curl
-brew install jenkins
-brew install openssl
-brew install libtool
-brew install cmake
-brew install automake
+brew install rmtrash
+#brew install wget
+#brew install curl
+#brew install jenkins
+#brew install openssl
+#brew install libtool
+#brew install cmake
+#brew install automake
 
 # Languages
-brew install rbenv
-brew install ruby-build
-brew cask install java
+brew install go
+#brew install rbenv
+#brew install ruby-build
+#brew cask install java
 
+brew install ghq
+brew install peco
 brew install md5sha1sum
 brew install nkf
-brew install sl
-brew install ctags
+#brew install sl
+#brew install ctags
 brew install tmux
-brew install fish
-brew install automeke
-brew install ag
-brew install z
-brew install php53-redis
-brew install redis
+#brew install fish
+#brew install ag
+#brew install z
+#brew install php53-redis
+#brew install redis
 
 # DevOps
 #brew install docker
 #brew cask install boot2docker
 #brew cask install packer
 
-# Casks
 brew cask install virtualbox
 brew cask install vagrant
-brew cask install svnx
+#brew cask install svnx
 brew cask install filezilla
-brew cask install firefox
-brew cask install openoffice
+#brew cask install firefox
+#brew cask install openoffice
+brew cask install atom
 brew cask install textmate
 brew cask install mamp
 brew cask install sourcetree
 brew cask install miro-video-converter
 brew cask install sequel-pro
-brew cask install kobito
+brew cask install spectacle
+#brew cask install kobito
 
-#brew cask install iterm2
+brew cask install iterm2
 #brew cask install google-chrome
 #brew cask install google-drive
 #brew cask install google-japanese-ime
